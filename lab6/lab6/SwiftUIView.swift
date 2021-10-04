3185292f1a14105b4561c0ac0c0322a6f815f84e
Macbook <macbook@Macbooks-MacBook.local>
2021-10-04 16:41:27 +0300

    lab6

---
+++ lab6/lab6/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  lab6
+//
+//  Created by Macbook on 27/02/1443 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
